87491fc2e8d9c57978434e1ac4d955cd348e035b
marudedameo2019 <marudedameo2019@users.noreply.github.com>
2026-07-05 13:44:54 +0900

    ベースとなる測定を実施し、mermaid用のlineデータを整形出力

---
+++ mermaid_base.sh
@@ -0,0 +1,20 @@
+#
+# ex) sh mermaid_base.sh rust_lazy_map_chain_bench.sh js_eager_map_chain_bench.sh js_lazy_map_chain_bench.sh
+FILES="$*"
+
+for f in $FILES; do
+    lang=$(echo $f | sed 's/_.*$//')
+    type=$(echo $f | sed 's/^[^_]*_\([^_]*\)_.*$/\1/')
+    log=$f.log
+    echo "[$f]"
+    sh $f | tee $f.log
+done
+
+for f in $FILES; do
+    lang=$(echo $f | sed 's/_.*$//')
+    type=$(echo $f | sed 's/^[^_]*_\([^_]*\)_.*$/\1/')
+    log=$f.log
+    echo -n "    line \"${lang}_${type}\" ["
+    sed 's/^[^:]*: //;s/ms$//' $log | tr '\n' ',' | sed 's/,$/]/'
+    echo
+done
